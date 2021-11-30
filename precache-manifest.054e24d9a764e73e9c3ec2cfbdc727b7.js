b673c38d63b305219789dbe646a63f629de8975f
Henry Jia <hjiausa@Henrys-MacBook-Pro.local>
2021-11-29 20:22:53 -0800

    Updates

---
+++ precache-manifest.054e24d9a764e73e9c3ec2cfbdc727b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bcb3044ffc3a89f306060bf41fe24a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75d62f5ec88d00de42b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75d62f5ec88d00de42b",
+    "url": "/static/js/main.f82cd963.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b6938fd7e9488b84df5a2f44025e088a",
+    "url": "/static/media/henryjia.b6938fd7.jpg"
+  },
+  {
+    "revision": "02966100c0de4696b05f5bfc3b556b74",
+    "url": "/static/media/resume.02966100.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.c9f8420d25d41a486a4d2a6ae13ce8a9.js"
+  "/precache-manifest.054e24d9a764e73e9c3ec2cfbdc727b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
